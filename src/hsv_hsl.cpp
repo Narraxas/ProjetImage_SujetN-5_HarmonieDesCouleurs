addc3a4aae3605abd821ac7ba758a44600b51939
renaud <75610434+RenaudDeroubaix@users.noreply.github.com>
2024-03-09 18:00:39 +0100

    tentative de monochromatique

--- src/hsv_hsl.cpp
+++ src/hsv_hsl.cpp
@@ -86,6 +86,32 @@ void HSL_to_RGB(OCTET *ImgOutRGB,vector<double> ImgInHSL, int nH, int nW){
     }
 }
 
+void monoChromatique(OCTET *ImgOutRGB,vector<double> ImgInHSL, int nH, int nW,double color){
+    double R,G,B;
+    for (int i=0;i<nH*nW*3;i+=3){
+        double H=color;
+        double S=ImgInHSL[i+1];
+        double L=ImgInHSL[i+2];
+        double X,C;
+        C=(1-fabs(2*L-1))*S;
+        H=H*360/60.0;
+        //X=C*(1-abs(H%2.0-1));
+        X=C*(1-fabs(fmod(H,2.0)-1));//fmod=% sur double
+        if(H>=0&&H<1){R=C;G=X;B=0;}
+        if(H>=1&&H<2){R=X;G=C;B=0;}
+        if(H>=2&&H<3){R=0;G=C;B=X;}
+        if(H>=3&&H<4){R=0;G=X;B=C;}
+        if(H>=4&&H<5){R=X;G=0;B=C;}
+        if(H>=5&&H<6){R=C;G=0;B=X;}
+        double m=L-C/2;
+        ImgOutRGB[i]=R+m>1?255:(R+m)*255.0;
+        ImgOutRGB[i+1]=G+m>1?255:(G+m)*255.0;
+        ImgOutRGB[i+2]=B+m>1?255:(B+m)*255.0;
+
+        
+    }
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -118,7 +144,7 @@ int main(int argc, char* argv[])
     vector<double> ImgHSL;
     ImgHSL.resize(nTaille3);
     RGB_to_HSL(ImgIn, ImgHSL, nH, nW);
-    HSL_to_RGB(ImgOut, ImgHSL, nH, nW);
+    monoChromatique(ImgOut, ImgHSL, nH, nW,0.75);
     for(int i=0;i<nTaille3;i++){
         //cout<<ImgHSL[i]*255<<endl;
         ImgOut2[i]=ImgHSL[i]*255;
